2ee424cd565eb1f3a5612355c16bac75391d4154
David Heredia <dherediat97@gmail.com>
2023-09-18 14:17:05 +0200

    Fix: typos, fix: theme in both modes(dark & light)

--- app/src/main/java/com/dherediat97/rickandmorty/ui/theme/Color.kt
+++ app/src/main/java/com/dherediat97/rickandmorty/ui/theme/Color.kt
@@ -4,5 +4,6 @@ import androidx.compose.ui.graphics.Color
 
 val Red = Color(0xFFFF5252)
 val Primary = Color(0xFF97CE4C)
+val Black = Color(0xFF000000)
 val Secondary = Color(0xFFF0E14A)
 val Tertiary = Color(0xFFE4A788)
